e4ed86ca65d935eb1d31b722d5b284fe83b5fbbd
Richard Obaze <richardobaze@yahoo.co.uk>
2026-02-24 18:49:14 +0000

    Setup project file structure, gitignore, github actions workflows and a basic server with a test endpoint

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Json, Router,
+    http::StatusCode,
+    routing::{get, post},
+};
+
+#[tokio::main]
+async fn main() {
+    let app = create_app();
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+// build our application with a route
+fn create_app() -> Router {
+    Router::new()
+        .route("/", get(root))
+        .route("/health_check", get(health_check))
+}
+
+async fn health_check() -> StatusCode {
+    StatusCode::OK
 }
